af8f960341beeb6ab37844f524c2a60bb28d48d0
Mohamed Abouzaid <119829381+mo-adel-tronic@users.noreply.github.com>
2025-12-06 10:32:46 +0200

    init app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   //  Here we are calling the Dependency Injection
   await DependencyInjection.init();
   //  This is the main app
--- lib/src/core/config/injection.dart
+++ lib/src/core/config/injection.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/material.dart';
-
 class DependencyInjection {
   static Future<void> init() async {
-    WidgetsFlutterBinding.ensureInitialized();
+    
   }
 }
