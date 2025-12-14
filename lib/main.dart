d0c38dd55b17a6ffe0696039752e829d93f8a5ac
Mohamed Abouzaid <119829381+mo-adel-tronic@users.noreply.github.com>
2025-12-14 08:57:50 +0200

    Delete lib/main.dart

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'src/core/config/config.dart';
-import 'package:flutter/material.dart';
-import 'app.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-   await DependencyInjection.init();
-   runApp(const MyApp());
-}
